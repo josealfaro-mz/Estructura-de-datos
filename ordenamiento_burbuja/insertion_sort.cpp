205b60ffa12562aff8815531340664fee9fbd191
josealfaro-mz <marix_pp@hotmail.com>
2026-09-23 10:21:56 -0600

    implementar algoritmo de ordenamiento por insercion

--- ordenamiento_burbuja/insertion_sort.cpp
+++ ordenamiento_burbuja/insertion_sort.cpp
@@ -7,8 +7,25 @@ void mostrarArreglo(int numeros[], int n)
     {
         cout << numeros[i] << " ";
     }
-
     cout << endl;
+}
+
+void insertionSort(int numeros[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int clave = numeros[i];
+        int j = i - 1;
+
+        // Desplaza los elementos de numeros[0..i-1] que son mayores que la clave
+        // a una posición adelante de su posición actual
+        while (j >= 0 && numeros[j] > clave)
+        {
+            numeros[j + 1] = numeros[j];
+            j--;
+        }
+        numeros[j + 1] = clave;
+    }
 }
 
 int main()
@@ -17,6 +34,12 @@ int main()
     int n = 7;
 
     cout << "Arreglo original:" << endl;
+    mostrarArreglo(numeros, n);
+
+    // Llamada al algoritmo de ordenamiento
+    insertionSort(numeros, n);
+
+    cout << "Arreglo ordenado:" << endl;
     mostrarArreglo(numeros, n);
 
     return 0;
